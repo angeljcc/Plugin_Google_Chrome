3b171f587fa28ac044cadd16545a4f3b569700d8
Angel Jiménez de Cisneros Carreño <angeljcc@gmail.com>
2015-06-28 20:07:21 +0200

    Initial commit

---
+++ background.js
@@ -0,0 +1,10 @@
+/**
+ * Created by angel on 28/06/15.
+ */
+// Copyright (c) 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+    console.log("he clicado");
+});

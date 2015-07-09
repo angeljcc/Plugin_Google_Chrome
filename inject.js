eaf2f3c77c640cee36450fe0da3ecf0b952dd2a3
Angel Jimenez de Cisneros Carreño <angeljcc@gmail.com>
2015-07-09 20:03:32 +0200

    Comentarios de cada función y eliminado codigo que no hace falta

--- inject.js
+++ inject.js
@@ -10,32 +10,31 @@
 
 
     var lista = new Array();
+
+    //I go over the ul and get into an array.
     $('#js_itemlist').each(function(indice, elemento) {
-        //console.log('El elemento con el índice '+indice+' contiene '+$(elemento).html());
         lista.push($(elemento).html());
 
     });
+    //Drop the ul in #js_itemlist to be putting our slide
     $("#js_itemlist").html("");
-    //$("#js_itemlist").append('<section class="filtergroup cf">Mi primer hack</section>');
     for(i=0;i<lista.length;i++) {
         //lista[i].next('.item_image').css()
        $("#js_itemlist").append(lista[i]);
     }
+       //Add classes that we need, and I create in CSS
         $(".item").addClass('item2');
         $(".item_wrapper").addClass('item_wrapper2');
         $(".item_image").addClass('item_image2');
-        //$(".item_image img").addClass('item_image2 img');
         $(".item_prices").addClass('item_prices2');
-        //$(".item_prices h3").addClass('item_prices2 h3');
 
+        //We remove classes that we do not need
         $(".item").removeClass('item');
-        //$(".item_wrapper").removeClass('item_wrapper');
         $(".item_image").removeClass('item_image');
-        //$(".item_image img").removeClass('item_image img');
         $(".item_prices").removeClass('item_prices');
+
+        //Now, built the slide to pass each item.
         var $objetoSlide = $('#js_itemlist');
-       // Oculto la ultima li que por defecto sale
-        $objetoSlide.find('li:gt(0)').hide();
 
         var $actual,$siguiente;
        if($objetoSlide.has('.Activa')){
